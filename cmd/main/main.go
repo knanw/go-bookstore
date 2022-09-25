1073a1b31d2443b44092f0d997e980f6c5b82b9f
wolfram.knan.geekadmin <wolfram.knan@geekinbusiness.de>
2022-09-25 09:06:06 +0200

    ic

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,10 +1,19 @@
 package main
 
-
 import (
 	"fms"
+	"log"
+	"net/http"
+
+	"github.com/akhil/go-bookstore/pkg/routes"
+	"github.com/gorilla/mux"
+	"github.com/jinzu/gorm/dialects/mysql"
+	"github.com/knanw/go-bookstore/pkg/routes"
 )
 
 func main()  {
-	
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r)) //create server on port 9010
 }
